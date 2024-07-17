96b8418202c959de70bf021acabe1d340933ea8f
Nabil KADOURI <kadourinabil7@gmail.com>
2024-07-17 12:05:57 +0200

    update

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -10,7 +10,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Filesystem\Filesystem;
 
 #[AsCommand(
     name: 'app:create-admin',
