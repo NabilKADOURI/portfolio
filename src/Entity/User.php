b0e31dd52d88a20cd6b3dd88fe42e01aebf62b56
NabilKADOURI <kadourinabil7@gmail.com>
2024-12-16 14:47:33 +0100

    update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,8 +66,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        
+        $roles[] = 'ROLE_ADMIN';
 
         return array_unique($roles);
     }
@@ -102,7 +102,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials(): void
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        
     }
 }
